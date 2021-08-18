d05d2348bf6b230d8bcbecfcc0ca6db35b6c8866
titanblue <titanblue@users.noreply.github.com>
2021-08-18 08:18:54 -0500

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-const productDetail = document.querySelector(".product-detail");
-document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${productDetail.dataset.pid}</h2><br><br>`);
