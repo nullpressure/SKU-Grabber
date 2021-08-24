16460151a83e7b9d303868df48d44510d9e410e3
titanblue <22titanblue@gmail.com>
2021-08-24 09:20:26 -0500

    delay

--- 510087.js
+++ 510087.js
@@ -1,14 +1,20 @@
-const targetNode = document.getElementById('product-detail-redesign');
-const config = { attributes: true, attributeFilter: ['data-pid'] };
-const regex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$|^\d\d\d\d\d\d$/;
-
-function callback(mutationList) {
-    mutationList.forEach(function(mutation) {
-        if (mutation.target[data-pid].match(regex)){
-            document.getElementById("skuInject").remove();
-            document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="skuInject"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${mutation.target[data-pid]}<br/></h2></div>`);
-        }
-    });
+function addIfExists() {
+    const targetNode = document.getElementById('product-detail-redesign');
+    if(!targetNode) {
+        window.setTimeout(addIfExists,100);
+        return;
+    }
+    const config = { attributes: true, attributeFilter: ['data-pid'] };
+    const regex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$|^\d\d\d\d\d\d$/;
+    function callback(mutationList) {
+        mutationList.forEach(function(mutation) {
+            if (mutation.target[data-pid].match(regex)){
+                document.getElementById("skuInject").remove();
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="skuInject"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${mutation.target[data-pid]}<br/></h2></div>`);
+            }
+        });
+    }
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
 }
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+addIfExists();
