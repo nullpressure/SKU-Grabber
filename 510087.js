ff76ccc4662c422a532dbd42f7b378649d795dc5
titanblue <22titanblue@gmail.com>
2021-08-24 09:12:32 -0500

    Simplified

--- 510087.js
+++ 510087.js
@@ -1,18 +1,14 @@
-function watchProductDetail(p) {
-    const productObserver = new MutationObserver(() => {
-        var skuInserted = document.getElementById("skuvar1");
-        if (skuInserted != null) {
-            document.getElementById("skuvar1").outerHTML = "";
-            document.getElementById("skuvar2").outerHTML = "";
+const targetNode = document.getElementById('product-detail-redesign');
+const config = { attributes: true, attributeFilter: ['data-pid'] };
+const regex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$|^\d\d\d\d\d\d$/;
+
+function callback(mutationList) {
+    mutationList.forEach(function(mutation) {
+        if (mutation.target[data-pid].match(regex)){
+            document.getElementById("skuInject").remove();
+            document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="skuInject"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${mutation.target[data-pid]}<br/></h2></div>`);
         }
-        document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<h2 class="product-name h2" id="skuvar1" style="color:red">SKU: </h2><h2 class="product-name h2" id="skuvar2">${p.dataset.pid}<br/></h2>`);
-        productObserver.disconnect();
     });
-    productObserver.observe(p, {attributes: true, attributeFilter: ['data-pid']});
 }
-watchProductDetail(document.getElementbyId("product-detail-redesign"));
-const replaceContainer = document.querySelector('.productDetailsReplaceContainer');
-const replaceObserver = new MutationObserver((records) => {
-  if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == 'product-detail-redesign'))) watchProductDetail(document.getElementbyId("product-detail-redesign"));
-});
-replaceObserver.observe(replaceContainer, {childList: true});
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
