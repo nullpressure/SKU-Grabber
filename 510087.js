91a6f5f98fe251a4e0cc45059f2d65e040db0a9b
titanblue <22titanblue@gmail.com>
2021-08-24 10:05:19 -0500

    Ready for Distro

--- 510087.js
+++ 510087.js
@@ -7,10 +7,13 @@ function addIfExists() {
     const config = { attributes: true, attributeFilter: ['data-pid'] };
     const regex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$|^\d\d\d\d\d\d$/;
     function callback(mutationList) {
-        mutationList.forEach(function(mutation) {
-            if (mutation.target[data-pid].match(regex)){
-                document.getElementById("skuInject").remove();
-                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="skuInject"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${mutation.target[data-pid]}<br/></h2></div>`);
+        mutationList.forEach( (mutation) => {
+            if (targetNode.getAttribute('data-pid').match(regex)){
+                var skuInserted = document.getElementById("sku-var");
+                if (skuInserted != null) {
+                    document.getElementById("sku-var").outerHTML = "";                
+                }
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:#red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
             }
         });
     }
@@ -18,3 +21,8 @@ function addIfExists() {
     observer.observe(targetNode, config);
 }
 addIfExists();
+const replaceContainer = document.querySelector('.productDetailsReplaceContainer');
+const replaceObserver = new MutationObserver((records) => {
+    if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == 'product-detail-redesign'))) addIfExists();
+});
+replaceObserver.observe(replaceContainer, {childList: true});
