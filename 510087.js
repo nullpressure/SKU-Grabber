db11ed95280b8ad193ed7950dc23ecfd98ba2f92
nullpressure <12737044+nullpressure@users.noreply.github.com>
2021-10-04 08:15:26 -0500

    bundle sku's

--- 510087.js
+++ 510087.js
@@ -1,11 +1,12 @@
 function addIfExists() {
     const targetNode = document.getElementById('product-detail-redesign');
     if(!targetNode) {
-        window.setTimeout(addIfExists,1);
+        window.setTimeout(addIfExists,100);
         return;
     }
     const config = { attributes: true, attributeFilter: ['data-pid'] };
-    const regex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$|^\d\d\d\d\d\d$/;
+    const regex = /^\d\d\d\d\d\d$/;
+    const bundleRegex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
     function callback(mutationList) {
         mutationList.forEach( (mutation) => {
             if (targetNode.getAttribute('data-pid').match(regex)){
@@ -13,6 +14,13 @@ function addIfExists() {
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
                 document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
+                return;
+            }else if (targetNode.getAttribute('data-pid').match(bundleRegex)){
+                try { 
+                    document.getElementById("sku-var").outerHTML = "";
+                } catch {};
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
+                return;
             }
         });
     }
