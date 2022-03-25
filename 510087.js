3f04f7c73d0784bd9caaded83923011f3e474cc9
nullpressure <12737044+nullpressure@users.noreply.github.com>
2022-03-25 10:15:55 -0500

    revert

--- 510087.js
+++ 510087.js
@@ -29,8 +29,8 @@ function addIfExists() {
     observer.observe(targetNode, config);
 }
 addIfExists();
-const replaceContainer = document.querySelector('[class=productDetailsReplaceContainer]');
+const replaceContainer = document.querySelector('.primary-details-row');
 const replaceObserver = new MutationObserver((records) => {
-    if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == '[class=productDetailsReplaceContainer]'))) addIfExists();
+    if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == '.primary-details-row'))) addIfExists();
 });
 replaceObserver.observe(replaceContainer, {childList: true});
