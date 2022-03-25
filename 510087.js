37f28f28431ba103887c4b57547a0fc19793463a
nullpressure <12737044+nullpressure@users.noreply.github.com>
2022-03-25 17:58:54 -0500

    maybe fixes oos

--- 510087.js
+++ 510087.js
@@ -1,5 +1,5 @@
 function addIfExists() {
-    const targetNode = document.querySelector('.btn-check-availability');
+    const targetNode = document.getElementById("add-to-cart");
     if(!targetNode) {
         window.setTimeout(addIfExists,100);
         return;
@@ -9,13 +9,13 @@ function addIfExists() {
     const bundleRegex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
     function callback(mutationList) {
         mutationList.forEach( (mutation) => {
-            if (targetNode.getAttribute('sku').match(regex)){
+            if (targetNode.getAttribute('data-pid').match(regex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
                 document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
                 return;
-            }else if (targetNode.getAttribute('sku').match(bundleRegex)){
+            }else if (targetNode.getAttribute('data-pid').match(bundleRegex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
