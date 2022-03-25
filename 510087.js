33ec9b69db7e1039123550998fd86830b884b454
nullpressure <12737044+nullpressure@users.noreply.github.com>
2022-03-25 18:01:02 -0500

    Update 510087.js

--- 510087.js
+++ 510087.js
@@ -1,25 +1,25 @@
 function addIfExists() {
-    const targetNode = document.getElementById("add-to-cart");
+    const targetNode = document.querySelector('.apple-pay-pdp');
     if(!targetNode) {
         window.setTimeout(addIfExists,100);
         return;
     }
-    const config = { attributes: true, attributeFilter: ['data-pid'] };
+    const config = { attributes: true, attributeFilter: ['sku'] };
     const regex = /^\d\d\d\d\d\d$/;
     const bundleRegex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
     function callback(mutationList) {
         mutationList.forEach( (mutation) => {
-            if (targetNode.getAttribute('data-pid').match(regex)){
+            if (targetNode.getAttribute('sku').match(regex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
-                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('sku')}<br/></h2></div>`);
                 return;
-            }else if (targetNode.getAttribute('data-pid').match(bundleRegex)){
+            }else if (targetNode.getAttribute('sku').match(bundleRegex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
-                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-pid')}<br/></h2></div>`);
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('sku')}<br/></h2></div>`);
                 return;
             }
         });
