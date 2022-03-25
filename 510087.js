6a364f43e907f1d38fbc30b690edcab665ce044b
nullpressure <12737044+nullpressure@users.noreply.github.com>
2022-03-25 17:41:37 -0500

    apple pay test

--- 510087.js
+++ 510087.js
@@ -1,26 +1,25 @@
 function addIfExists() {
-    const targetNode = document.getElementById('product-detail-redesign');
-
+    const targetNode = document.querySelector('.apple-pay-pdp');
     if(!targetNode) {
         window.setTimeout(addIfExists,100);
         return;
     }
-    const config = { attributes: true, attributeFilter: ['data-selected-variant-pid'] };
+    const config = { attributes: true, attributeFilter: ['sku'] };
     const regex = /^\d\d\d\d\d\d$/;
     const bundleRegex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
     function callback(mutationList) {
         mutationList.forEach( (mutation) => {
-            if (targetNode.getAttribute('data-selected-variant-pid').match(regex)){
+            if (targetNode.getAttribute('data-pid').match(regex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
-                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-selected-variant-pid')}<br/></h2></div>`);
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('sku')}<br/></h2></div>`);
                 return;
-            }else if (targetNode.getAttribute('data-selected-variant-pid').match(bundleRegex)){
+            }else if (targetNode.getAttribute('data-pid').match(bundleRegex)){
                 try { 
                     document.getElementById("sku-var").outerHTML = "";
                 } catch {};
-                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('data-selected-variant-pid')}<br/></h2></div>`);
+                document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetNode.getAttribute('sku')}<br/></h2></div>`);
                 return;
             }
         });
@@ -32,5 +31,5 @@ addIfExists();
 const replaceContainer = document.querySelector('.productDetailsReplaceContainer');
 const replaceObserver = new MutationObserver((records) => {
     if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == 'product-detail-redesign'))) addIfExists();
-    });
+});
 replaceObserver.observe(replaceContainer, {childList: true});
