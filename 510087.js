f493854260b1e5adf41c7fcbc9f69bad5afece33
nullpressure <12737044+nullpressure@users.noreply.github.com>
2022-04-06 16:59:09 -0500

    finally got it

--- 510087.js
+++ 510087.js
@@ -1,57 +1,47 @@
 function addIfExists() {
-    
-    //const regex = /^\d\d\d\d\d\d$/;
-    //const bundleRegex = /^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
-
-    const targetNode = document.querySelector("div[Selected='Selected']");
-    const targetAttr = document.getElementById("product-detail-redesign").dataset.pid;
-    const applePaySKU = console.log(document.querySelector("isapplepay").getAttribute('sku'));
-    if(!targetNode&&!targetAttr&&!applePaySKU) {
-        setTimeout(addIfExists,100);
+    if(document.readyState !== "complete") {
+        setTimeout(addIfExists,10);
         return;
     }
-    
-    const targetSKU = targetNode.dataset.conditionSelectedVariantId;
-    
-
-    console.log("targetSKU: ",targetSKU);
-    console.log("targetAttr: ",targetAttr);
-    console.log("applePaySKU: ",applePaySKU);
-    //const config = { attributes: true, attributeFilter: ['data-condition-selected-variant-id'] };
-    //function callback(mutationList) {
-    //    mutationList.forEach( (mutation) => {
     const regex = /^\d\d\d\d\d\d$|^[a-zA-z]\d\d\d\d\d\d[a-zA-z]$/;
-    if (targetSKU.match(regex)){
-        try { 
-            document.getElementById("sku-var").outerHTML = "";
-        } catch {};
-        document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetSKU}<br/></h2></div>`);
-        return;
-    }else if (targetAttr.match(regex)){
-        try { 
-            document.getElementById("sku-var").outerHTML = "";
-        } catch {};
-        document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetAttr}<br/></h2></div>`);
-        return;
-    }else if (applePaySKU.match(regex))
-    try { 
-        document.getElementById("sku-var").outerHTML = "";
-    } catch {};
-    document.querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${applePaySKU}<br/></h2></div>`);
-    return;
-}
-    //    });
-    //}
-    //const observer = new MutationObserver(callback);
-    //observer.observe(targetNode, config);
-
-addIfExists();
+    try {
+        const targetSKU = document.querySelector("div[Selected='Selected']").getAttribute('condition-selected-variant-id');
+        if (targetSKU.match(regex)){
+            try { 
+                document.getElementById("sku-var").outerHTML = "";
+            } catch {};
+            document.getElementById('primary-details').querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetSKU}<br/></h2></div>`);
+            return;
+        }
+    } catch{
+        try{
+            const applePaySKU = document.querySelector("isapplepay").getAttribute('sku');
+            if (applePaySKU.match(regex)){
+                try { 
+                    document.getElementById("sku-var").outerHTML = "";
+                } catch {};
+                document.getElementById('primary-details').querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${applePaySKU}<br/></h2></div>`);
+                return;
+            }
+        } catch {
+            try {
+                const targetAttr = document.getElementById("product-detail-redesign").dataset.pid;
+                if (targetAttr.match(regex)){
+                    try { 
+                        document.getElementById("sku-var").outerHTML = "";
+                    } catch {};
+                    document.getElementById('primary-details').querySelector(".product-name").insertAdjacentHTML("beforebegin", `<div id="sku-var"><h2 class="product-name h2" style="color:red">SKU: </h2><h2 class="product-name h2">${targetAttr}<br/></h2></div>`);
+                    return;
+                }       
+             } catch{
+                setTimeout(addIfExists,10);
+                return;
+                }
+            }
+        }
 const replaceContainer = document.querySelector('.productDetailsReplaceContainer');
 const replaceObserver = new MutationObserver((records) => {
     if (records.some(r => r.type == 'childList' && r.addedNodes && Array.from(r.addedNodes).some(e => e.id == 'product-detail-redesign'))) addIfExists();
 });
 replaceObserver.observe(replaceContainer, {childList: true});
-
-//var s = new XMLSerializer();
-//s.serializeToString((document.querySelector('.productDetailsReplaceContainer')));
-//var checkForBundleSKU =  "dw.ac._capture({id: \""
+}addIfExists();
